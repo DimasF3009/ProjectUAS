7f04d833cd999e62519ce4988994032e1efabab4
dimas <wenz.id12@gmail.com>
2024-01-07 00:01:41 +0700

    commit

--- asset/code/ci4/app/Views/footer_view.php
+++ asset/code/ci4/app/Views/footer_view.php
@@ -1,6 +1,6 @@
             <br>
             <footer class="container">
-                <p>&copy; 2024 - Universitas Pelita Bangsa | Dimas Firmansyah - 312210267 -  TI.22.A.2</p>
+                <p>&copy; 2024 - Universitas Pelita Bangsa | Kelompok 11 -  TI.22.A.2</p>
             </footer>
         </div>
     </body>
