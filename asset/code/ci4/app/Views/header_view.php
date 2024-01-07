ace2aeffd6ac9716e3516aec75e3286f9d660ee9
dimas <wenz.id12@gmail.com>
2024-01-07 18:27:42 +0700

    commit

--- asset/code/ci4/app/Views/header_view.php
+++ asset/code/ci4/app/Views/header_view.php
@@ -11,25 +11,26 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container">
+        <div class="container-fluid">
             <a class="navbar-brand" href="<?= base_url();?>">Data Warga RT 03 RW 08<br>Kelurahan SukaSari</a>
-            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                aria-expanded="false" aria-label="Toggle navigation"></button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="/warga">Warga</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/iuran">Data Iuran</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/laporan">Laporan</a>
-					</li>
-				</ul>
-			</div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" href="/warga">Warga</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="/iuran">Data Iuran</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="/laporan">Laporan</a>
+                </li>
+              </ul>
+            </div>
         </div>
     </nav>
     
